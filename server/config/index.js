8a19d5ba7a212829968385cc5d67570d20fd1e60
winer110 <1834170330@qq.com>
2018-02-07 11:38:21 +0800

    add ecosystem.json

--- server/config/index.js
+++ server/config/index.js
@@ -6,11 +6,7 @@ export default {
 		// 正式
 		// appID:'wx6392df89290f5291',
 		// appsecret:'24fd2e9f671bd58cfc0adbab9497999a',
-		// token:'ahab2017',
-		// 测试号
-		// appID:'wx72c82a00fbd2d006',
-		// appsecret:'6b6675e5c70381c76caf1ec43c5be76d',
-		// token:'genee'
+		// token:'ahab2017',		
 		
 		// 18404904826@163.com test
 		// appID:'wx2c5cf038c2e8b7ec',
@@ -21,6 +17,11 @@ export default {
 		appID:'wx02a308e55e02aacc',
 		appSecret:'4792bd29dd09c4e347aea8e819685c28',
 		token:'datian'
+
+		// 测试号
+		// appID:'wx72c82a00fbd2d006',
+		// appsecret:'6b6675e5c70381c76caf1ec43c5be76d',
+		// token:'datian'
 	}
 	
 }
